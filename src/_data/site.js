6876e37b5b37143589cfe2dec689c72df118df47
Kaushalendra Kumar <91857508+Kaushalendra01@users.noreply.github.com>
2022-10-22 19:58:28 +0530

    New Website

---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'HTML Tutorial',
+  description: 'Eleventy starter project to create a blog',
+  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  // your site url without trailing slash
+  url: 'https://html-tutorial.netlify.app',
+  // how many posts you want to show for each page
+  paginate: 6
+  // if you want to add disqus to your site
+  // disqusShortname: "your-shortname"
+};
